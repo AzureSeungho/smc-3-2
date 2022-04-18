2befea21f7586b21f6c57c8c199b1818d572c1f1
Administrator <Administrator@501-S10>
2022-04-18 09:11:00 +0900

    0418 추가

---
+++ 0418/src/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum {
+	public static void main(String args[]) {
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("시작 숫자 : ");
+		int a = sc.nextInt();
+		System.out.print("마지막 숫자: ");
+		int b = sc.nextInt();
+		for(int i = a;i<=b;i++) {
+			sum += i;
+		}
+		System.out.println(a + "부터 " + b + "까지의 합은 " + sum);
+	}
+}
